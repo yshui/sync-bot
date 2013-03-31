1ac42abfd3d708621c4ad84a917cc142d646c150
Yuxuan Shui <yshuiv7@gmail.com>
2013-03-31 22:41:56 +0800

    initial commit

---
+++ check.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use POSIX;
+my $pid;
+if($pid=fork){
+	die "Can't fork: $!\n" if $pid<0;
+	exit 0;
+}
+POSIX::setsid or die "setsid: $!\n";
+if($pid=fork){
+	die "Can't fork: $!\n" if $pid<0;
+	exit 0;
+}
+umask 0;
+foreach (0 .. (POSIX::sysconf (&POSIX::_SC_OPEN_MAX) || 1024))
+	{ POSIX::close $_ }
+open (STDIN, "</dev/null");
+open (STDERR, ">&STDOUT");
+chdir "/";
+my $base_dir="/fsdata/site/mirror/";
+my $status_dir=$base_dir.".status/";
+open (STDOUT, ">>${status_dir}.checker.log");
+opendir my $sdh, $status_dir or die "Failed to open $status_dir\n";
+opendir my $dh, $base_dir or die "Failed to open $base_dir\n";
+while(1){eval{
+	rewinddir $dh;
+	rewinddir $sdh;
+	my @managed_repos = grep { !/\./ && -d "$status_dir/$_" } readdir($sdh);
+	my @all_repos = grep { !/\./ && -d "$base_dir/$_" } readdir($dh);
+	print "@all_repos";
+	my %mask=();
+	for my $tmp (@managed_repos){
+		$mask{$tmp}=1;
+	}
+	my @status;
+	for my $tmp (@all_repos){
+		my %s=();
+		$s{name}=$tmp;
+		$s{managed} = exists($mask{$tmp});
+		if($s{managed}){
+			my $lname = $status_dir.$tmp."/lock";
+			my $ename = $status_dir.$tmp."/error";
+			my $sname = $status_dir.$tmp."/synctime";
+			if(-e $ename){
+				$s{status}="failed";
+				open my $errfile, "<", $ename or die "Can't open $ename\n";
+				my $code = <$errfile>;
+				chomp $code;
+				$s{err}=$code;
+				close $errfile;
+			}elsif(-e $lname){
+				open my $lockfile, "<", $lname or die "Can't open $lname\n";
+				my $lockpid = <$lockfile>;
+				chomp $lockpid;
+				if(!(kill 0, $lockpid)){
+					$s{status}="weird";
+				}else{
+					$s{status}="updating";
+				}
+				close $lockfile;
+			}elsif(!-e $sname){
+				$s{status}="nosyncfile";
+			}else{
+				$s{status}="updated";
+				open my $syncfile, "<", $sname or die "Can't open $sname\n";
+				$s{synctime}=<$syncfile>;
+				chomp $s{synctime};
+				close $syncfile;
+			}
+		};
+		push @status, \%s;
+	}
+	print "@status";
+	my $stname=$base_dir."mirror_status.json";
+	open my $statusfile, ">", $stname;
+	#Hand written json, haha
+	print $statusfile "[";
+	my $first=1;
+	for my $tmp (@status){
+		my %a=%{$tmp};
+		if(!$first){
+			print $statusfile ",";
+		}else{
+			$first=0;
+		}
+		print $statusfile '{';
+		print $statusfile qq/"name":"$a{name}"/;
+		print $statusfile ',"managed":', $a{managed}?"true":"false";
+		if($a{managed}){
+			print $statusfile qq/,"err":$a{err}/ if($a{status} eq "failed");
+			print $statusfile qq/,"synctime":$a{synctime}/ if(exists($a{synctime}));
+			print $statusfile qq/,"status":"$a{status}"/;
+		}
+		print $statusfile '}';
+	}
+	print $statusfile "]";
+	close $statusfile;
+};
+	if($@){
+		print "Update status failed with $@\n";
+	}else{
+		print "mirror_status.json updated at ", time, "\n";
+	}
+	sleep 5*60;
+}
+
---
+++ sync.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Fcntl qw(:flock SEEK_END);
+use File::Basename;
+#First, get the arguments
+#my $uri=shift or die "You must specify a rsync uri\n";
+my $name=shift or die "You must specify a destination\n";
+my $max_retries=shift || 5;
+my $mirror_base_dir="/fsdata/site/mirror/";
+my $mirror_status_dir=$mirror_base_dir.".status/";
+if (!($uri =~ /\/$/)){
+	print "Warning: $uri is not ended with a slash\n";
+}
+die "Mirror name shouldn't contain slashes\n" if $name =~ /\//;
+my $dest=$mirror_base_dir.$name."/";
+my $status=$mirror_status_dir.$name."/";
+if (! -e $status){
+	mkdir $status or die "Can't create stauts dir $status for $name\n";
+}
+die "Sync source isn't specified, can't find ${status}source" if (! -e $status."source");
+open my $source, "<", $status."source";
+my $uri = <$source>;
+close $source;
+chomp $uri;
+#acquire lock file
+my $lockfile;
+if (-e $status."lock"){
+	open $lockfile, "<", $status."lock" or die "can't open ${status}lock for read\n";
+	my $pid=undef;
+	while(<$lockfile>){
+		chomp;
+		if(/^\d+$/){
+			$pid=$_;
+			last;
+		}
+	}
+	my $ex = kill 0, $pid;
+	die "Another instance is running, $pid\n" if $ex;
+	close $lockfile;
+}
+open $lockfile, ">", $status."lock" or die "can't open ${status}lock for write\n";
+flock $lockfile, LOCK_EX or die "Can't lock file ${status}lock\n";
+print $lockfile "$$\n";
+#--delay-updates isn't necessery if we have ZFS/BtrFS, sigh
+my $default_rsync_options_0='-azHP';
+my $default_rsync_options=' -6 --delete-after --no-motd --safe-links --delay-updates --out-format="%n%L" ';
+if (-e $status."exclude"){
+	$default_rsync_options.="--exclude-from=${status}exclude";
+}
+if (-e $status."override_options"){
+	open my $opt, "<", $status."override_options";
+	my $tmp = <$opt>;
+	chomp $tmp;
+	if (/(.*-){2,}/){
+		#a simple sanity check, prevent abusing of override_options
+		die "There should be only one dash in override_options\n";
+	}
+	$default_rsync_options_0 = $tmp;
+	close $opt;
+}
+$default_rsync_options = $default_rsync_options_0.$default_rsync_options;
+#print $default_rsync_options;
+my $last_exit_code;
+while($max_retries--){
+	print "$max_retries\n";
+	open my $res, "rsync $default_rsync_options $uri $dest |";
+	my $flag=0;
+	while(<$res>){
+		chomp;
+		my $line=$_;
+		print "$line\n";
+		if(!($line =~ /^\s*$/)){
+			#there're changes
+			$flag=1;
+		}
+	}
+	close $res;
+	$last_exit_code = $?;
+	print "exit code: $last_exit_code\n";
+	if ($last_exit_code){
+		$flag = 1;
+	}
+	#If there're changes/errors, run again.
+	last if(!$flag);
+}
+if($last_exit_code != 0){
+	#Error occurs
+	open my $error, ">", $status."error";
+	print $error "$last_exit_code";
+}else{
+	if (-e $status."error"){
+		unlink $status."error";
+	}
+	open my $syncfile, ">", $status."synctime";
+	flock $syncfile, LOCK_EX;
+	print $syncfile time, "\n";
+	flock $syncfile, LOCK_UN;
+	close $syncfile;
+}
+flock $lockfile, LOCK_UN;
+close $lockfile;
+unlink $status."lock";
